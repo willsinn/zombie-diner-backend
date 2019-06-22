4dcaeef843ef400b3e8ca15d9e9b3e17940f3bd2
Gamerman2001 <Tro12185@gmail.com>
2019-06-22 18:43:53 -0400

    added images to ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_22_173510) do
+ActiveRecord::Schema.define(version: 2019_06_22_224011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_06_22_173510) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "orders", force: :cascade do |t|
