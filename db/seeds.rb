f80a6509e88789ce551aea3aa80b70154cb802a1
William Sinn <w.h.sinn@gmail.com>
2019-06-22 14:55:54 -0400

    add recipes, agreed to remove models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,34 @@
-Ingredient.create(name:"limbs")
-Ingredient.create(name:"eyes")
-Ingredient.create(name:"brains")
-Ingredient.create(name:"organs")
+# Ingredients
+Ingredient.create(name:"Limbs")
+Ingredient.create(name:"Eyes")
+Ingredient.create(name:"Brains")
+Ingredient.create(name:"Organs")
 
-Recipe.create(name:"brainwich")
-Recipe.create(name:"eyes")
-Recipe.create(name:"limbkibab")
+# Recipe Names
+Recipe.create(name:"Brain Brulee")
+# brain
+Recipe.create(name:"Eyeburger")
+# eyes
+Recipe.create(name:"Limb Kebabs")
+# limbs
 
+Recipe.create(name:"Spaghetti and Eyeballs")
+# organs and eyes
+Recipe.create(name:"Keylimb Eye")
+# limbs and eyes
+Recipe.create(name:"Limbone Steak")
+# limbs and organs
+Recipe.create(name:"Pulled Limb Sandwich")
+# limbs and brains
+
+Recipe.create(name:"Organs and Brains over Rice")
+# organs, brains, eyes, limbs
+Recipe.create(name:"Brainbutter and Organjelly Sandwich")
+# organs, brains, and limbs
+Recipe.create(name:"Brains wrapped in Organs over Eyes")
+# organs, brains, and eyes
+
+# Recipes
 RecipeIngredient.create(ingredient_id: 2 , recipe_id: 0 )
 RecipeIngredient.create(ingredient_id: 1, recipe_id: 0 )
 RecipeIngredient.create(ingredient_id: 1, recipe_id: 1)
