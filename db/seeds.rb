ead5ed96fab2883fdf14968add44bc6d302657ce
Edgar Ong <emailedgar@gmail.com>
2019-06-22 20:08:47 -0400

    updated assets

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,35 @@
 # Ingredients
-limbs = Ingredient.create(name:"Limbs", image: 'assets/limbs.svg')
-eyes = Ingredient.create(name:"Eyes", image: 'assets/eyes.svg')
-brains = Ingredient.create(name:"Brains", image: 'assets/brains.svg')
-organs = Ingredient.create(name:"Organs", image: 'assets/organs.svg')
+limbs = Ingredient.create(name:"Limbs", image: 'assets/ingredients/limbs.svg')
+eyes = Ingredient.create(name:"Eyes", image: 'assets/ingredients/eyes.svg')
+brains = Ingredient.create(name:"Brains", image: 'assets/ingredients/brains.svg')
+organs = Ingredient.create(name:"Organs", image: 'assets/ingredients/organs.svg')
 
 
 # Recipe Names
-brain_brulee = Recipe.create(name:"Brain Brulee")
+brain_brulee = Recipe.create(name:"Brain Brulee", image:'assets/recipes/brulee.svg')
 # brain
-eye_burger = Recipe.create(name:"Eyeburger")
+eye_burger = Recipe.create(name:"Eyeburger", image:'assets/recipes/burger.svg')
 # eyes
-limb_kebab = Recipe.create(name:"Limb Kebabs")
+limb_kebab = Recipe.create(name:"Limb Kebabs", image:'assets/recipes/kebab.svg')
 # limbs
 
-spaghetti_eyeballs = Recipe.create(name:"Spaghetti and Eyeballs")
+spaghetti_eyeballs = Recipe.create(name:"Spaghetti and Eyeballs", image:'assets/recipes/spaghetti.svg')
 # organs and eyes
-key_limb_eye = Recipe.create(name:"Keylimb Eye")
+key_limb_eye = Recipe.create(name:"Keylimb Eye", image:'assets/recipes/pie.svg')
 # limbs and eyes
-limbone_steak = Recipe.create(name:"Limbone Steak")
+limbone_steak = Recipe.create(name:"Limbone Steak", image:'assets/recipes/steak.svg')
 # limbs and organs
-pulled_limb_sandwich = Recipe.create(name:"Pulled Limb Sandwich")
+pulled_limb_sandwich = Recipe.create(name:"Pulled Limb Sandwich", image:'assets/recipes/sandwich.svg')
 # limbs and brains
 
-organs_brains_rice = Recipe.create(name:"Organs and Brains over Rice")
+organs_brains_rice = Recipe.create(name:"Organs and Brains over Rice", image:'assets/recipes/rice.svg')
 # organs, brains, eyes, limbs
-brain_butter_organ_jelly = Recipe.create(name:"Brainbutter and Organjelly Sandwich")
+brain_butter_organ_jelly = Recipe.create(name:"Brainbutter and Organjelly Sandwich", image:'assets/recipes/jelly.svg')
 # organs, brains, and limbs
-brain_wrapped_organ_over_eyes = Recipe.create(name:"Brains wrapped in Organs over Eyes")
+brain_wrapped_organ_over_eyes = Recipe.create(name:"Brains wrapped in Organs over Eyes", image:'assets/recipes/bacon.svg')
 # organs, brains, and eyes
 
-# Recipes.... make sure to drop DB  and 
+# Recipes.... make sure to drop DB  and
 RecipeIngredient.create(ingredient_id: limbs.id , recipe_id: limb_kebab.id )
 RecipeIngredient.create(ingredient_id: brains.id , recipe_id: brain_brulee.id )
 RecipeIngredient.create(ingredient_id: eyes.id , recipe_id: eye_burger.id)
