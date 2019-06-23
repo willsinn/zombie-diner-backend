6bbfb34983ebe9538f2edc96d7d95237c5723d8d
Jacob Lam <jlam18@binghamton.edu>
2019-06-23 10:39:34 -0400

    food instances

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'recipes', to: 'recipes#index'
   get 'ingredients', to: 'recipes#show'
+  
+  get 'orders', to: 'recipes#orders'
+  get 'foods', to: 'recipes#foods'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
